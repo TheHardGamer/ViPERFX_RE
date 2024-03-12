e111a480a91f15df1cbb7d8673b5aaac8c1b5a98
TheHardGamer <gamelovr695@gmail.com>
2024-03-12 16:01:45 +0530

    Disable all logging

--- src/log.h
+++ src/log.h
@@ -4,6 +4,6 @@
 
 #define TAG "ViPER4Android"
 
-#define VIPER_LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
-#define VIPER_LOGI(...) __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
-#define VIPER_LOGE(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
+#define VIPER_LOGD(...) 
+#define VIPER_LOGI(...) 
+#define VIPER_LOGE(...)
